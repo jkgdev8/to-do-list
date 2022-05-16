5dd737ab44dafa4e0783ba5da040d73af39582fe
jkgdev8 <jkgdev8@gmail.com>
2022-05-15 17:10:48 -0700

    todo list

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
-// Use state hook
-import React, { useState } from "react";
+// useState => allows you to have state variables in functional components. In React you never want to modify a state variable, always make a copy with the ... operator
+// useRef = allows us to reference elements to our html.
+// useEffect => allows you to perform side effects in your components.
+import React, { useState, useRef, useEffect } from "react";
 import Todolist from "./Todolist";
+import { v4 as uuidv4 } from 'uuid';
 
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 // First place to start is at the app.js this is the root of your application
 function App() {
-  const [todos, setTodos] = useState([{id: 1, name: 'Todo 1', complete: false}])
-  // can only return one thing
+  const [todos, setTodos] = useState([])
+  const todoNameref = useRef()
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem
+    (LOCAL_STORAGE_KEY))
+
+    if (storedTodos) setTodos(storedTodos)
+  },[])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+
+  }, [todos])
+
+  function toggleTodo(id){
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  function handleAddTodo(e){
+    const name = todoNameref.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
+    })    
+    todoNameref.current.value = null
+
+  }
+
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
   return (
     <>
-      // Props      
-      <Todolist todos = {todos}/>
-      <input type='text' />
-      <button>Add Todo</button>
-      <button> Clear Complete</button>
-      <div> 0 left to do</div>
+       
+      <Todolist todos = {todos} toggleTodo={toggleTodo}/>
+      <input ref={todoNameref} type='text' />
+      <button onClick = {handleAddTodo}>Add Todo</button>
+      <button onClick = {handleClearTodos}> Clear Complete</button>
+      <div> {todos.filter(todo => !todo.complete).length} Left to do</div>
     </>
   )
   
--- src/Todolist.js
+++ src/Todolist.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Todo from './Todo'
 
-export default function Todolist( { todos} ) {
+export default function Todolist( { todos, toggleTodo} ) {
   
   return (
     todos.map(todo => {
-      return <Todo key = {todo.id} todo = {todo} />
+      return <Todo key = {todo.id} toggleTodo={toggleTodo} todo = {todo} />
     })
   )
 }
